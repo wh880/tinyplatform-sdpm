6cb0c14a832d540372d8da6dc64e20445dde1e14
Hulk <jayzch@qq.com>
2015-08-31 20:48:03 +0800

    完善菜单

--- sdpm-common/common/src/main/java/org/tinygroup/sdpm/menu/MenuFileProcessor.java
+++ sdpm-common/common/src/main/java/org/tinygroup/sdpm/menu/MenuFileProcessor.java
@@ -18,34 +18,31 @@ public class MenuFileProcessor extends AbstractFileProcessor {
     private MenuManager menuManager;
 
     public void process() {
-        {
-            XStream stream = XStreamFactory.getXStream(MenuManager.XSTREAN_PACKAGE_NAME);
-            for (FileObject fileObject : deleteList) {
-                logger.logMessage(LogLevel.INFO, "正在删除菜单menu文件[{0}]",
-                        fileObject.getAbsolutePath());
-                Menu menu = (Menu) caches.remove(fileObject.getAbsolutePath());
-                if (menu != null) {
-                    menuManager.removeMenu(menu.getName());
-                    caches.remove(fileObject.getAbsolutePath());
-                }
-                logger.logMessage(LogLevel.INFO, "删除逻辑菜单menu文件[{0}]结束",
-                        fileObject.getAbsolutePath());
+        XStream stream = XStreamFactory.getXStream(MenuManager.XSTREAN_PACKAGE_NAME);
+        for (FileObject fileObject : deleteList) {
+            logger.logMessage(LogLevel.INFO, "正在删除菜单menu文件[{0}]",
+                    fileObject.getAbsolutePath());
+            Menu menu = (Menu) caches.remove(fileObject.getAbsolutePath());
+            if (menu != null) {
+                menuManager.removeMenu(menu.getId());
+                caches.remove(fileObject.getAbsolutePath());
             }
-            for (FileObject fileObject : changeList) {
-                logger.logMessage(LogLevel.INFO, "正在读取菜单menu文件[{0}]",
-                        fileObject.getAbsolutePath());
-                Menu oldMenu = (Menu) caches.get(fileObject.getAbsolutePath());
-                if (oldMenu != null) {
-                    menuManager.removeMenu(oldMenu.getName());
-                }
-                Menu menu = (Menu) stream.fromXML(fileObject.getInputStream());
-                menuManager.addMenu(menu);
-                caches.put(fileObject.getAbsolutePath(), menu);
-                logger.logMessage(LogLevel.INFO, "读取菜单menu文件[{0}]结束",
-                        fileObject.getAbsolutePath());
-            }
+            logger.logMessage(LogLevel.INFO, "删除逻辑菜单menu文件[{0}]结束",
+                    fileObject.getAbsolutePath());
         }
-
+        for (FileObject fileObject : changeList) {
+            logger.logMessage(LogLevel.INFO, "正在读取菜单menu文件[{0}]",
+                    fileObject.getAbsolutePath());
+            Menu oldMenu = (Menu) caches.get(fileObject.getAbsolutePath());
+            if (oldMenu != null) {
+                menuManager.removeMenu(oldMenu.getId());
+            }
+            Menu menu = (Menu) stream.fromXML(fileObject.getInputStream());
+            menuManager.addMenu(menu, fileObject.getAbsolutePath());
+            caches.put(fileObject.getAbsolutePath(), menu);
+            logger.logMessage(LogLevel.INFO, "读取菜单menu文件[{0}]结束",
+                    fileObject.getAbsolutePath());
+        }
     }
 
     @Override
--- sdpm-common/common/src/main/java/org/tinygroup/sdpm/menu/impl/MenuManagerImpl.java
+++ sdpm-common/common/src/main/java/org/tinygroup/sdpm/menu/impl/MenuManagerImpl.java
@@ -6,48 +6,55 @@ import org.tinygroup.logger.LoggerFactory;
 import org.tinygroup.sdpm.menu.Menu;
 import org.tinygroup.sdpm.menu.MenuManager;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MenuManagerImpl implements MenuManager {
 
     /**
-     * 所有菜单的数组
-     */
-    private List<Menu> menus = new ArrayList<Menu>();
-    /**
      * 所有菜单的Map
      */
-    private Map<String, Menu> menuMap = new HashMap<String, Menu>();
+    private static Map<String, Menu> menuMap = new HashMap<String, Menu>();
     private static Logger logger = LoggerFactory.getLogger(MenuManagerImpl.class);
 
-    public void addMenu(Menu menu) {
-        String menuName = menu.getName();
-        logger.logMessage(LogLevel.DEBUG, "添加菜单:{}", menuName);
-        if (menuMap.containsKey(menuName)) {
-            logger.logMessage(LogLevel.WARN, "菜单:{}已存在,删除已有菜单", menuName);
-            removeMenu(menuName);
+    public void addMenu(Menu menu, String fileName) {
+        String menuId = menu.getId();
+        logger.logMessage(LogLevel.DEBUG, "添加菜单:[id:{},name:{}]", menuId, menu.getName());
+        if (menuMap.containsKey(menuId)) {
+            logger.logMessage(LogLevel.ERROR, "菜单:[id:{},name:{}]已存在,重复定义在文件{}", menuId, menu.getName(), fileName);
+            return;
         }
-        menus.add(menu);
         menuMap.put(menu.getName(), menu);
-        logger.logMessage(LogLevel.WARN, "菜单:{}添加完毕", menuName);
-
+        logger.logMessage(LogLevel.WARN, "菜单:[id:{},name:{}]", menuId, menu.getName());
+        for (Menu child : menu.getChildMenus()) {
+            child.setParentId(menu.getId());
+            addMenu(child, fileName);
+        }
     }
 
-    public void removeMenu(String name) {
-        logger.logMessage(LogLevel.DEBUG, "删除菜单:{}", name);
-        if (!menuMap.containsKey(name)) {
-            logger.logMessage(LogLevel.WARN, "菜单:{}不存在,无须删除", name);
+    public void removeMenu(String menuId) {
+        logger.logMessage(LogLevel.DEBUG, "删除菜单id:{}", menuId);
+        if (!menuMap.containsKey(menuId)) {
+            logger.logMessage(LogLevel.WARN, "菜单menuId:{}不存在,无须删除", menuId);
         } else {
-            menus.remove(menuMap.remove(name));
+            menuMap.remove(menuId);
         }
-        logger.logMessage(LogLevel.DEBUG, "删除菜单:{}完毕", name);
+        logger.logMessage(LogLevel.DEBUG, "删除菜单menuId:{}完毕", menuId);
+    }
+
+    public List<Menu> getChildMenus(String parentId) {
+        Menu menu = menuMap.get(parentId);
+        if (menu != null) {
+            return menu.getChildMenus();
+        }
+        return null;
+    }
+
+    public Menu getMenu(String menuId) {
+        return menuMap.get(menuId);
     }
 
     public List<Menu> getMenus() {
-        return menus;
+        return new ArrayList<Menu>(menuMap.values());
     }
 
 }
