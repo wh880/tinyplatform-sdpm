f61865b5422fe0666708331365f2563602ccfde1
xubinbin <1835094758@qq.com>
2015-09-11 13:32:37 +0800

    新增字典

---
+++ sdpm-system/view/src/main/resources/system/page/dictionaries/data/tree/dict.js
@@ -0,0 +1,11 @@
+[
+		{ id: 4, pId: 0, name: "性别", open: true, nocheck: true },
+		{ id: 7, pId: 0, name: "民族", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "教育程度", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "残疾等级", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "残疾类别", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "角色类型", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "用户状态", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "岗位任职规则属性", open: true, nocheck: true },
+		{ id: 15, pId: 0, name: "岗位职责规则属性", open: true, nocheck: true }
+]
