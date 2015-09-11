9649a76c5449c6ea47add55cc8b691992e8ae13e
沈立宇 <1245492902@qq.com>
2015-09-11 15:14:22 +0800

    增加删除按钮封装宏

--- sdpm-common/view/src/main/resources/common/js/sdpm.js
+++ sdpm-common/view/src/main/resources/common/js/sdpm.js
@@ -8,4 +8,34 @@ $(function () {
             objtip.text(msg);
         },
     });
+    //删除按钮功能
+    $("#tinypagecontent").on("click", "[data-removeid]", function (e) {
+        var url = $(this).attr("href") ? $(this).attr("href") : window.location.href;
+        $.ajax({
+            url: $(this).attr("data-action"),
+            data: {action: "del", id: $(this).attr("data-removeid")},
+            dataType: "json",
+            success: function (data) {
+                if (data.status == "success") {
+                    layer.msg(data.info);
+                    setTimeout(function () {
+                        window.location.href = url;
+                    }, 300)
+                } else {
+                    layer.msg(data.info);
+                }
+
+            },
+            error: function () {
+                layer.msg("error")
+            }
+        })
+        return false;
+        /*var url=$(this).attr("href");
+         layer.confirm("ddddd",function(){
+         window.location.href=url;
+         // console.log("fdsf")
+         });
+         return false;*/
+    });
 });
