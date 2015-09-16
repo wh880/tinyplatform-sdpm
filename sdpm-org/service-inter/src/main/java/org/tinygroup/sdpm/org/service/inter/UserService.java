055cbcbb4b73a446a041bcb2a96f29a37d137e22
Hulk <jayzch@qq.com>
2015-09-16 21:07:14 +0800

    修改字段

--- sdpm-org/service-inter/src/main/java/org/tinygroup/sdpm/org/service/inter/UserService.java
+++ sdpm-org/service-inter/src/main/java/org/tinygroup/sdpm/org/service/inter/UserService.java
@@ -13,13 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.tinygroup;
+package org.tinygroup.sdpm.org.service.inter;
 
-public class HelloWorldServiceImpl implements HelloWorldService {
-    public String sayHello(String name) {
-        if (name == null) {
-            name = "world.";
-        }
-        return "hello," + name;
-    }
+public interface UserService {
+    public String sayHello(String name);
 }
---
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/org/UserAction.java
@@ -0,0 +1,11 @@
+package org.tinygroup.sdpm.action.org;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 用户管理控制器
+ * Created by Hulk on 2015/9/16.
+ */
+@Controller
+public class UserAction {
+}
