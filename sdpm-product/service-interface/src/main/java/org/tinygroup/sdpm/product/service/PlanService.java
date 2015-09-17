94c408826142285fd99521055dbfce966fe2946a
汪杰 <wangjie14929@hundsun.com>
2015-09-17 11:50:47 +0800

    修改

--- sdpm-product/service-interface/src/main/java/org/tinygroup/sdpm/product/service/PlanService.java
+++ sdpm-product/service-interface/src/main/java/org/tinygroup/sdpm/product/service/PlanService.java
@@ -3,55 +3,80 @@ package org.tinygroup.sdpm.product.service;
 import java.util.List;
 
 import org.tinygroup.sdpm.product.dao.pojo.Plan;
+import org.tinygroup.sdpm.product.dao.pojo.Story;
+import org.tinygroup.tinysqldsl.Pager;
 
 
 public interface PlanService {
 	/**
-	 * 创建计划
-	 * @param plan
+	 * 创建计划 
+	 * @param Plan
 	 * @return
 	 */
 	Plan add(Plan plan);
 	
 	/**
-	 * 编辑计划
+	 * 保存计划
+	 * @param Plan
+	 * @return
+	 */
+	Plan save(Plan plan);
+	
+	/**
+	 * 根据计划ID查找计划
+	 * @param Plan
+	 * @return
+	 */
+	Plan findByPlanId(Integer PlanId);
+	/**
+	 * 根据计划名称查找需求
+	 * @param Plan
+	 * @return
+	 */
+	List<Story> findByPlanName(String planName);
+	
+	/**
+	 * 根据计划ID编辑计划
+	 * @param Plan
+	 * @return
+	 */
+	int edit(Integer PlanId);
+	
+	/**
+	 * 根据计划ID删除计划
+	 * @param Plan
+	 * @return
+	 */
+	int deleteByPlanId(Integer planId);
+	
+	/**
+	 * 根据计划ID查找关联需求
+	 * @param Stories
+	 * @return
+	 */
+	List<Story> find(Integer planId);
+/*	
+	*//**
+	 * 根据计划ID查找关联BUG
+	 * @param BUGs
+	 * @return
+	 *//*
+	List<Bug> findByPlanId(Integer planId);*/
+	/**
+	 * 根据计划对象分页查询
+	 * @param currentPage
+	 * @param limit
 	 * @param plan
 	 * @return
 	 */
-	int eidt(Plan plan);
+	Pager<Plan> findPager(int currentPage,int limit ,Plan plan);
 	
 	/**
-	 * 根据计划对象删除
-	 * @param plan
+	 * 根据计划Id批量删除
+	 * @param PlanIds
 	 * @return
 	 */
-	int deleteByPlan(Plan plan);
-
-	/**
-	 * 根据计划批量删除
-	 * @param plan
-	 * @return
-	 */
-	int batchDelete(List<Plan> plans);
+	int deleteByPlanIds(Integer... planIds);
 	
-	/**
-	 * 根据计划对象删除
-	 * @param plan
-	 * @return
-	 */
-	int deleteById(Integer planId);
 	
-	/**
-	 * 根据计划Id查找日志
-	 * @param id
-	 * @return
-	 */
-	List<Plan> findByPlanId(Integer planId);
-	
-	/**
-	 * 根据计划ID查找
-	 * @param id
-	 * @return
-	 */
-	Plan findById(Integer planId);
 }
