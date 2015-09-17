474a7d0e4f14175072d4222633c0fb54b6e2f79a
chenpeng <cp786156072@163.com>
2015-09-17 14:11:18 +0800

    测试服务接口

--- sdpm-quality/service-interface/src/main/java/org/tinygroup/sdpm/quality/service/inter/TestTaskService.java
+++ sdpm-quality/service-interface/src/main/java/org/tinygroup/sdpm/quality/service/inter/TestTaskService.java
@@ -1,5 +1,44 @@
 package org.tinygroup.sdpm.quality.service.inter;
 
-public class TestTaskService {
+import java.util.List;
 
+import org.tinygroup.sdpm.quality.dao.pojo.TestTask;
+
+public interface TestTaskService {
+	/**
+	 * 通过状态查询
+	 * @param testtask
+	 * @return
+	 */
+	List<TestTask> findByStatus(String testtask);
+	/**
+	 * 提交测试
+	 * @param testtask
+	 * @return
+	 */
+	TestTask add(TestTask testtask);
+	/**
+	 * 编辑测试
+	 * @param testtask
+	 * @return
+	 */
+	int update(TestTask testtask);
+	/**
+	 * 删除测试
+	 * @param id
+	 * @return
+	 */
+	int deleteById(Integer id);
+	/**
+	 * 需求关联查询
+	 * @param testtask
+	 * @return
+	 */
+	List<TestTask> findByLinkStory(TestTask testtask);
+	/**
+	 * Bug关联查询
+	 * @param testtask
+	 * @return
+	 */
+	List<TestTask> findByLinkBug(TestTask testtask);
 }
