29db253be80dedd741b9bbe1269cdd1dfa30b72e
王莹 <wymsmdbl@163.com>
2015-09-18 17:35:19 +0800

    修改文件大小写

--- sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/TaskEsTimateServiceImpl.java
+++ sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/TaskEsTimateServiceImpl.java
@@ -2,12 +2,12 @@ package org.tinygroup.sdpm.project.service.impl;
 
 
 import org.springframework.stereotype.Component;
-import org.tinygroup.sdpm.project.service.inter.TaskEsTimateService;
+import org.tinygroup.sdpm.project.service.inter.TaskEsTimateService2;
 
 /**
  * Created by wangying14938 on 2015-09-18.
  */
 
 @Component
-public class TaskEsTimateServiceImpl implements TaskEsTimateService {
+public class TaskEsTimateServiceImpl implements TaskEsTimateService2 {
 }
--- sdpm-project/service-interface/src/main/java/org/tinygroup/sdpm/project/service/inter/TaskEsTimateService2.java
+++ sdpm-project/service-interface/src/main/java/org/tinygroup/sdpm/project/service/inter/TaskEsTimateService2.java
@@ -3,5 +3,5 @@ package org.tinygroup.sdpm.project.service.inter;
 /**
  * Created by shenly13343 on 2015-09-17.
  */
-public interface TaskEsTimateService {
+public interface TaskEsTimateService2 {
 }
