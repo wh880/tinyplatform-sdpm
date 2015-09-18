05572258b7847df07af6cc6029e48ee984bd0713
wll <wllaixyn@163.com>
2015-09-18 20:49:48 +0800

    sql工具修正

--- sdpm-common/common/src/main/java/org/tinygroup/util/sql/SqlUtil.java
+++ sdpm-common/common/src/main/java/org/tinygroup/util/sql/SqlUtil.java
@@ -9,26 +9,73 @@ public class SqlUtil {
 
     public String toSql(List<SearchInfo> searchInfos){
         StringBuffer sql = new StringBuffer();
+        int size = searchInfos.size();
+        int nullMark = 0;
         SearchInfo searchInfo;
-        for(int i = 0 ;i<searchInfos.size();i++){
-            searchInfo = searchInfos.get(i);
-            if(searchInfo.getField()!=null&&!"".equals(searchInfo.getField())){
-                sql.append(searchInfo.getRelation()==null||"".equals(searchInfo.getRelation())?" ":" "+searchInfo.getRelation());
-                if(i%3 == 0){
-                    sql.append("((").append(seanSearchInfoAppend(searchInfo));
-                }else if(i%3 == 1){
-                    sql.append(seanSearchInfoAppend(searchInfo)).append(")");
+        int num =(size/3)+ (size%3==0?0:1);
+        int total[] = totalFields(searchInfos,num);
+        for(int i = 0;i<num;i++){
+            int count = 0;
+            for(int j = 0 ;j<3;j++){
+                count++;
+                searchInfo = searchInfos.get(j+3*i);
+                if(searchInfo.getField()!=null&&!"".equals(searchInfo.getField())){
+                    sql.append(searchInfo.getRelation()==null||"".equals(searchInfo.getRelation())?" ":" "+searchInfo.getRelation());
+                    if(nullMark == 1){
+                        if(total[i] - j >0){
+                            sql.append(" (");
+                        }
+                    }
+                    if((j+3*i)%3 == 0){
+                        if(total[i] == 3){
+                            sql.append("((");
+                        }else if(total[i] == 2){
+                            sql.append("(");
+                        }
+                        sql.append(seanSearchInfoAppend(searchInfo));
+                    }else if((j+3*i)%3 == 1){
+                        String end = "";
+                        if(!sql.toString().endsWith("(")&&count>1){
+                            end = ")";
+                        }
+                        sql.append(seanSearchInfoAppend(searchInfo)+end);
+                    }else{
+                        String end = "";
+                        if(!sql.toString().endsWith("(")&&count>1){
+                            end = ")";
+                        }
+                        sql.append(seanSearchInfoAppend(searchInfo)+end);
+                    }
+                    nullMark = 0;
                 }else{
-                    sql.append(seanSearchInfoAppend(searchInfo)).append(")");
+                    if(total[i] -j> 0){
+                        nullMark = 1;
+                    }
                 }
             }
         }
+
         return sql.toString();
     }
 
     public String seanSearchInfoAppend( SearchInfo searchInfo){
         StringBuffer sql = new StringBuffer();
         return sql.append(" "+searchInfo.getField()).append(" "+searchInfo.getOperate()).append(" "+searchInfo.getValue()).toString();
+    }
+
+    public int[] totalFields(List<SearchInfo> searchInfos, int n){
+        int result[] = new int[n];
+        for(int i = 0;i<n;i++) {
+            int count = 0;
+            for (int j = 0; j < 3; j++) {
+                SearchInfo searchInfo = searchInfos.get(j+3*i);
+                if(searchInfo.getField()!=null&&!"".equals(searchInfo.getField())){
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        return result;
     }
 
 }
