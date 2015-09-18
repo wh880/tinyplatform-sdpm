0bf4f4325429732326c4a13355b371c45c026f45
沈立宇 <1245492902@qq.com>
2015-09-18 16:36:26 +0800

    增加业务、服务接口及实现文件，整理pom依赖

--- sdpm-project/service-interface/src/main/java/org/tinygroup/sdpm/project/service/inter/ProjectService.java
+++ sdpm-project/service-interface/src/main/java/org/tinygroup/sdpm/project/service/inter/ProjectService.java
@@ -1,7 +1,50 @@
 package org.tinygroup.sdpm.project.service.inter;
 
+import org.tinygroup.sdpm.project.dao.pojo.Project;
+
+import java.util.List;
+
 /**
  * Created by shenly13343 on 2015-09-17.
  */
 public interface ProjectService {
+    /**
+     * 新增项目，保证项目名称唯一
+     *
+     * @param project
+     * @return
+     */
+    public Project add(Project project);
+
+    /**
+     * 查找所有项目
+     *
+     * @return
+     */
+    public List<Project> findList();
+
+    /**
+     * 根据项目名称查找项目，项目名称要保证唯一
+     *
+     * @param projectName
+     * @return
+     */
+    public Project findByName(String projectName);
+
+    /**
+     * 根据项目id查找
+     *
+     * @param projectId
+     * @return
+     */
+    public Project findById(int projectId);
+
+    /**
+     * 产品id查询所有关联的项目id，放入list
+     * 根据项目id查询数据
+     *
+     * @param list
+     * @return
+     */
+    public List<Project> findByProjectList(List<Integer> list);
 }
