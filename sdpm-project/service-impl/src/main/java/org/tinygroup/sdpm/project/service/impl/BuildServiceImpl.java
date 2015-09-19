707ade09f1995c04a11b51c57e854b27b3c6a4f6
沈立宇 <1245492902@qq.com>
2015-09-20 01:16:42 +0800

    回复文件大小写

--- sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/BuildServiceImpl.java
+++ sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/BuildServiceImpl.java
@@ -1,9 +1,19 @@
 package org.tinygroup.sdpm.project.service.impl;
 
+import org.tinygroup.sdpm.project.dao.pojo.Build;
 import org.tinygroup.sdpm.project.service.inter.BuildService;
+import org.tinygroup.tinysqldsl.Pager;
 
 /**
  * Created by shenly13343 on 2015-09-20.
  */
 public class BuildServiceImpl implements BuildService {
+
+    public Build add(Build build) {
+        return null;
+    }
+
+    public Pager<Build> findPager(int page, int pagesize, int projectId) {
+        return null;
+    }
 }
--- sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/TaskServiceImpl.java
+++ sdpm-project/service-impl/src/main/java/org/tinygroup/sdpm/project/service/impl/TaskServiceImpl.java
@@ -1,9 +1,45 @@
 package org.tinygroup.sdpm.project.service.impl;
 
+import org.tinygroup.sdpm.project.dao.pojo.Task;
 import org.tinygroup.sdpm.project.service.inter.TaskService;
+import org.tinygroup.tinysqldsl.Pager;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by shenly13343 on 2015-09-20.
  */
 public class TaskServiceImpl implements TaskService {
+    public Task save(Task task) {
+        return null;
+    }
+
+    public Pager<Task> findList() {
+        return null;
+    }
+
+    public Map<String, List<Task>> findByGroup(String colum) {
+        return null;
+    }
+
+    public Pager<Task> findByStatus(String status) {
+        return null;
+    }
+
+    public Task update(Task task) {
+        return null;
+    }
+
+    public Pager<Task> findComplex() {
+        return null;
+    }
+
+    public void ouputFile() {
+
+    }
+
+    public void input() {
+
+    }
 }
