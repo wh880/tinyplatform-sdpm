8ae66363bbd017486c100c9c9f5f9f8e3c361cc2
沈立宇 <1245492902@qq.com>
2015-09-20 00:23:30 +0800

    新增文件

---
+++ sdpm-project/service-interface/src/main/java/org/tinygroup/sdpm/project/service/inter/ProjectProductService.java
@@ -0,0 +1,37 @@
+package org.tinygroup.sdpm.project.service.inter;
+
+import java.util.List;
+
+/**
+ * Created by shenly13343 on 2015-09-17.
+ */
+public interface ProjectProductService {
+    /**
+     * 通过projectId获得productId列表
+     * @param porjectId
+     * @return
+     */
+    public List<Integer> findProducts(int porjectId);
+
+    /**
+     * 通过prodcutId获得projectId列表
+     * @param productId
+     * @return
+     */
+    public List<Integer> findProjects(int productId);
+
+    /**
+     * 增加product和project映射
+     * @param projectId
+     * @param productId
+     * @return
+     */
+    public int save(int projectId, int productId);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public int deleteById(int id);
+}
