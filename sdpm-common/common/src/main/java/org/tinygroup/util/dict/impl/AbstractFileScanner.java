e5bb53c1abbb5e56c2baedab9903785ea04c5852
wll <wllaixyn@163.com>
2015-09-20 21:16:18 +0800

    删除metadata测试代码，修正表字段读取代码

--- sdpm-common/common/src/main/java/org/tinygroup/util/dict/impl/AbstractFileScanner.java
+++ sdpm-common/common/src/main/java/org/tinygroup/util/dict/impl/AbstractFileScanner.java
@@ -2,6 +2,7 @@ package org.tinygroup.util.dict.impl;
 
 import org.tinygroup.logger.Logger;
 import org.tinygroup.logger.LoggerFactory;
+
 import org.tinygroup.util.dict.inter.CallBackFunction;
 import org.tinygroup.util.dict.inter.FileScanner;
 import org.tinygroup.vfs.FileObject;
@@ -16,9 +17,18 @@ public abstract class AbstractFileScanner implements FileScanner {
             .getLogger(AbstractFileScanner.class);
 
     public void resolverFile(FileObject file, CallBackFunction callBackFunction) {
-        if(file.isFolder()&&!file.getFileName().equals("test-classes")){
+        if(file.isFolder()&&!file.getFileName().contains("classes")){
             for(FileObject f:file.getChildren()){
-                resolverFile(f,callBackFunction);
+                if(file.getFileName().contains(".jar")){
+                    if(file.getFileName().matches("sdpm-metadata*\\.jar")){
+                        resolverFile(f,callBackFunction);
+                    }else{
+                        continue;
+                    }
+                }else {
+                    resolverFile(f,callBackFunction);
+                }
+
             }
         }else {
             callBackFunction.process(file);
