8d864b5c1433e5d720f889353b2110aea7715ebc
蒙初开 <721787227@qq.com>
2015-09-20 11:20:20 +0800

    common 添加基本的业务接口

--- sdpm-common/business-interface/src/main/java/org/tinygroup/sdpm/common/biz/inter/SysModuleManager.java
+++ sdpm-common/business-interface/src/main/java/org/tinygroup/sdpm/common/biz/inter/SysModuleManager.java
@@ -1,5 +1,33 @@
 package org.tinygroup.sdpm.common.biz.inter;
 
-public interface SysModuleManager {
+import java.util.List;
 
+import org.tinygroup.sdpm.common.dao.pojo.SysModule;
+
+public interface SysModuleManager {
+	/**
+	 * 编辑
+	 * @param sysModule
+	 * @return
+	 */
+	 SysModule edit(SysModule sysModule);
+	 
+	/**
+	 * 删除ById
+	 * @param id
+	 */
+	 int deleteById(int id);
+	
+	/**
+	 * 根据根节点进行查询
+	 * @param root
+	 * @return
+	 */
+	 List<SysModule> findByRoot(int root);
+	 /**
+	  * 通过对象进行删除
+	  * @param sysModule
+	  * @return
+	  */
+	 int delete(SysModule sysModule);
 }
--- sdpm-common/service-interface/src/main/java/org/tinygroup/sdpm/common/service/inter/SysModuleService.java
+++ sdpm-common/service-interface/src/main/java/org/tinygroup/sdpm/common/service/inter/SysModuleService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.tinygroup.sdpm.common.dao.pojo.SysModule;
 
 public interface SysModuleService {
+	
 	/**
 	 * 编辑
 	 * @param sysModule
