13b8f5547fce5561ffb318784446841dc490a358
蒙初开 <721787227@qq.com>
2015-09-21 11:03:42 +0800

    修改接口和实现

--- sdpm-common/service-impl/src/main/java/org/tinygroup/sdpm/common/service/impl/EffortServiceImpl.java
+++ sdpm-common/service-impl/src/main/java/org/tinygroup/sdpm/common/service/impl/EffortServiceImpl.java
@@ -3,45 +3,53 @@ package org.tinygroup.sdpm.common.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.tinygroup.sdpm.common.biz.inter.EffortManager;
 import org.tinygroup.sdpm.common.dao.pojo.Effort;
 import org.tinygroup.sdpm.common.service.inter.EffortService;
 import org.tinygroup.tinysqldsl.Pager;
 
 public class EffortServiceImpl implements EffortService {
-
+    private EffortManager  effortManager;
 	public List<Effort> findByDate(Date date) {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Auto-generated method stub 
+		
+		return effortManager.findByDate(date);
 	}
 
 	public Effort save(Effort effort) {
 		// TODO Auto-generated method stub
-		return null;
+		if(effort.getEffortId()==null){
+		return effortManager.add(effort);
+		}
+		else{
+			int i=effortManager.updata(effort);
+			return null;
+		}
 	}
 
 	public List<Effort> findByAccount(String account) {
 		// TODO Auto-generated method stub
-		return null;
+		return effortManager.findByAccount(account);
 	}
 
 	public List<Effort> find(Effort effort) {
 		// TODO Auto-generated method stub
-		return null;
+		return effortManager.find(effort);
 	}
 
 	public List<Effort> findBetweenDate(Date begindate, Date enddate) {
 		// TODO Auto-generated method stub
-		return null;
+		return effortManager.findBetweenDate(begindate, enddate);
 	}
 
 	public List<Effort> findByProject(int projectId) {
 		// TODO Auto-generated method stub
-		return null;
+		return effortManager.findByProject(projectId);
 	}
 
 	public Pager<Effort> findByPage(int start, int limit, Effort effort) {
 		// TODO Auto-generated method stub
-		return null;
+		return effortManager.findByPage(start, limit, effort);
 	}
 
 }
