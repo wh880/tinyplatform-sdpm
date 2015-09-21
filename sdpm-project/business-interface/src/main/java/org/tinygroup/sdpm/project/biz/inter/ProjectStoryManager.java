92fb200746c150e44b5de03a14f2dcd091604e33
沈立宇 <1245492902@qq.com>
2015-09-21 11:30:11 +0800

    新增项目需求关联表

---
+++ sdpm-project/business-interface/src/main/java/org/tinygroup/sdpm/project/biz/inter/ProjectStoryManager.java
@@ -0,0 +1,52 @@
+package org.tinygroup.sdpm.project.biz.inter;
+
+import org.tinygroup.sdpm.project.dao.pojo.ProjectProduct;
+import org.tinygroup.sdpm.project.dao.pojo.ProjectStory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shenly13343 on 2015-09-21.
+ */
+public interface ProjectStoryManager {
+    /**
+     * 根据projectId查找关联产品
+     *
+     * @param projectStory 对象
+     * @return
+     */
+    ArrayList<Integer> findArrayStory(ProjectStory projectStory);
+
+    /**
+     * 根据条件查询List
+     *
+     * @param projectStory 用于查询条件
+     * @return
+     */
+    List<ProjectProduct> findList(ProjectStory projectStory);
+
+    /**
+     * 新增关联
+     *
+     * @param projectProduct
+     * @return
+     */
+    ProjectProduct add(ProjectProduct projectProduct);
+
+    /**
+     * 更新用户
+     *
+     * @param projectproduct 需要更新的实体类
+     * @return
+     */
+    Integer update(ProjectProduct projectproduct);
+
+    /**
+     * 根据id进行删除
+     *
+     * @param id 主键
+     * @return
+     */
+    Integer delete(int id);
+}
