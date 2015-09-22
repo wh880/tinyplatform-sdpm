f75490caa2fde5a7c5eb4d286759ff8fe835d8ad
沈立宇 <1245492902@qq.com>
2015-09-22 14:28:35 +0800

    文件恢复

---
+++ sdpm-org/service-wrapper/src/main/java/org/tinygroup/sdpm/org/wrapper/UserServiceImplWrapper.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
+ * <p/>
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.gnu.org/licenses/gpl.html
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tinygroup.sdpm.org.wrapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.tinygroup.cepcore.CEPCore;
+import org.tinygroup.context.Context;
+import org.tinygroup.context.impl.ContextImpl;
+import org.tinygroup.event.Event;
+import org.tinygroup.event.Parameter;
+import org.tinygroup.event.ServiceInfo;
+import org.tinygroup.event.ServiceRequest;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component("userService")
+public class UserServiceImplWrapper implements org.tinygroup.sdpm.org.service.inter.UserService {
+
+    @Autowired
+    CEPCore core;
+
+    public CEPCore getCore() {
+        return core;
+    }
+
+    public void setCore(CEPCore core) {
+        this.core = core;
+    }
+
+    private Event getEvent(String serviceId, Context context) throws Exception {
+        Event event = new Event();
+        event.setEventId(UUID.randomUUID().toString());
+        ServiceRequest serviceRequest = new ServiceRequest();
+        serviceRequest.setContext(context);
+        serviceRequest.setServiceId(serviceId);
+        event.setServiceRequest(serviceRequest);
+        return event;
+    }
+
+    public org.tinygroup.sdpm.org.dao.pojo.OrgUser findUser(java.lang.String id) {
+        String serviceId = "org_findUser";
+
+        try {
+            Context context = new ContextImpl();
+            context.put("id", id);
+
+            return callServiceAndCallBack(serviceId, context);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("服务[%s]发生异常", serviceId), e);
+        }
+    }
+
+    public java.util.List<org.tinygroup.sdpm.org.dao.pojo.OrgUser> findUserList(org.tinygroup.sdpm.org.dao.pojo.OrgUser orgUser) {
+        String serviceId = "org_findUserList";
+
+        try {
+            Context context = new ContextImpl();
+            context.put("orgUser", orgUser);
+
+            return callServiceAndCallBack(serviceId, context);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("服务[%s]发生异常", serviceId), e);
+        }
+    }
+
+    public org.tinygroup.sdpm.org.dao.pojo.OrgUser addUser(org.tinygroup.sdpm.org.dao.pojo.OrgUser orgUser) {
+        String serviceId = "org_addUser";
+
+        try {
+            Context context = new ContextImpl();
+            context.put("orgUser", orgUser);
+
+            return callServiceAndCallBack(serviceId, context);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("服务[%s]发生异常", serviceId), e);
+        }
+    }
+
+    public org.tinygroup.sdpm.org.dao.pojo.OrgUser updateUser(org.tinygroup.sdpm.org.dao.pojo.OrgUser orgUser) {
+        String serviceId = "org_updateUser";
+
+        try {
+            Context context = new ContextImpl();
+            context.put("orgUser", orgUser);
+
+            return callServiceAndCallBack(serviceId, context);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("服务[%s]发生异常", serviceId), e);
+        }
+    }
+
+    public java.lang.Integer deleteUser(java.lang.String id) {
+        String serviceId = "org_deleteUser";
+
+        try {
+            Context context = new ContextImpl();
+            context.put("id", id);
+
+            return callServiceAndCallBack(serviceId, context);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("服务[%s]发生异常", serviceId), e);
+        }
+    }
+
+    private <T> T callServiceAndCallBack(String serviceId, Context context) throws Exception {
+        Event event = getEvent(serviceId, context);
+        core.process(event);
+        ServiceInfo info = core.getServiceInfo(serviceId);
+        List<Parameter> resultsParam = info.getResults();
+        if (resultsParam == null || resultsParam.size() == 0) {
+            return null;
+        }
+        return event.getServiceRequest().getContext()
+                .get(resultsParam.get(0).getName());
+    }
+
+}
