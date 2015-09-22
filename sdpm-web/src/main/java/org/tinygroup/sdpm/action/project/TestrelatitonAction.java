54ceca46e5bd0214e84ae3c1276df37408916a18
沈立宇 <1245492902@qq.com>
2015-09-22 16:25:06 +0800

    类名错误

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/project/TestrelatitonAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/project/TestrelatitonAction.java
@@ -5,5 +5,5 @@ import org.tinygroup.sdpm.common.web.BaseController;
 /**
  * Created by wangying14938 on 2015-09-22.用户关系（甘特图）
  */
-public class TßestrelatitonAction extends BaseController {
+public class TestrelatitonAction extends BaseController {
 }
