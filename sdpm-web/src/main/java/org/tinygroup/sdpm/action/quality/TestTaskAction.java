67cdcee0bd2ddca845fc3eaf065c544a32e5925a
chenpeng <cp786156072@163.com>
2015-09-22 16:17:21 +0800

    1

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/TestTaskAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/TestTaskAction.java
@@ -1,5 +1,7 @@
 package org.tinygroup.sdpm.action.quality;
 
-public class TestTaskAction {
+import org.tinygroup.sdpm.common.web.BaseController;
+
+public class TestTaskAction extends BaseController {
 
 }
