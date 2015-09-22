cd9d6589742b4e6a388f40ef822ad1db7b214ece
沈立宇 <1245492902@qq.com>
2015-09-22 17:00:19 +0800

    修复注解错误

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/project/TestrelatitonAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/project/TestrelatitonAction.java
@@ -1,9 +1,14 @@
 package org.tinygroup.sdpm.action.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.tinygroup.sdpm.common.web.BaseController;
+import org.tinygroup.sdpm.project.service.inter.TaskRelationService;
 
 /**
  * Created by wangying14938 on 2015-09-22.用户关系（甘特图）
  */
 public class TestrelatitonAction extends BaseController {
+    @Autowired
+    private TaskRelationService taskRelationService;
+
 }
