300bd369ac598bfed73a208001f2125684ba573a
沈立宇 <1245492902@qq.com>
2015-09-22 15:26:50 +0800

    修改quality action

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/BugAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/BugAction.java
@@ -1,13 +1,8 @@
 package org.tinygroup.sdpm.action.quality;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.tinygroup.sdpm.common.web.BaseController;
-import org.tinygroup.sdpm.quality.dao.pojo.Bug;
-import org.tinygroup.sdpm.quality.service.inter.BugService;
 
 /**
  * Created by chenpeng15668 on 2015-9-22
@@ -16,12 +11,12 @@ import org.tinygroup.sdpm.quality.service.inter.BugService;
 @Controller
 @RequestMapping("/quality/bug")
 public class BugAction extends BaseController {
-
-	@Autowired
-	private BugService bugService;
-	
-	@RequestMapping(value = "/save",method = RequestMethod.POST)
-	public String save(Bug bug,Model model){
-		return "testManagement/page/Bug.page";
-	}
+//
+//	@Autowired
+//	private BugService bugService;
+//
+//	@RequestMapping(value = "/save",method = RequestMethod.POST)
+//	public String save(Bug bug,Model model){
+//		return "testManagement/page/Bug.page";
+//	}
 }
