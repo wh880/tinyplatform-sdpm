bc91c200ecbd475d2e19fc59cff4036bbe57909c
wll <wllaixyn@163.com>
2015-09-25 10:19:20 +0800

    product story 相关

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/StoryAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/StoryAction.java
@@ -34,6 +34,7 @@ public class StoryAction {
     @RequestMapping("/search")
     public String storySearchAction(int page, int limit, ProductStory story,String groupOperate, SearchInfos searchInfos, String sortName, String asc, Model model, HttpServletRequest request){
         Pager<ProductStory> p = storyService.findStoryPager(limit*(page - 1),limit,story,searchInfos,groupOperate,sortName,"asc".equals(asc)?true:false);
+        model.addAttribute("storyList",p);
         return "product/data/tabledata.pagelet";
     }
 }
