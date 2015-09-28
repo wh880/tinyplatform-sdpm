a015920c86a4d4e80b46776ed0bcf1408df2d816
xubinbin <1835094758@qq.com>
2015-09-28 20:10:23 +0800

    xiugai

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/PlanAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/PlanAction.java
@@ -66,7 +66,6 @@ public class PlanAction  extends BaseController{
 
 	@RequestMapping("/list")
 	public String list(ProductPlan plan,
-
 			@RequestParam(required = false,defaultValue = "1")int page,
 			@RequestParam(required = false,defaultValue = "10")int pagesize,
 			@RequestParam(required = false,defaultValue = "planId")String order,
@@ -76,7 +75,7 @@ public class PlanAction  extends BaseController{
 
 		model.addAttribute("productPlan",pagerProductPlan);
 
-		return "/product/data/allproduct-release.pagelet";
+		return "/product/data/allproduct-plan.pagelet";
 	}
 
 	
