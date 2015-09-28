762c81b7d8bc1093129d18a760eced2eddef27e3
wll <wllaixyn@163.com>
2015-09-28 17:38:34 +0800

    product相关，字典处理

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/ProductAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/ProductAction.java
@@ -1,13 +1,17 @@
 package org.tinygroup.sdpm.action.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.tinygroup.sdpm.common.web.BaseController;
 import org.tinygroup.sdpm.product.dao.pojo.Product;
 import org.tinygroup.sdpm.product.service.ProductService;
-import org.tinygroup.weblayer.mvc.annotation.Controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 产品控制器
@@ -15,11 +19,20 @@ import org.tinygroup.weblayer.mvc.annotation.Controller;
  *
  */
 @Controller
-@RequestMapping("/product")
+@RequestMapping("product")
 public class ProductAction extends BaseController{
 	
 	@Autowired
 	private ProductService productService;
+	@RequestMapping("")
+	public String productAction(HttpServletRequest request){
+		List<Product> list = (List<Product>) request.getSession().getAttribute("productList");
+		if(list == null){
+			list = productService.findProductList(new Product(),"productId","desc");
+			request.getSession().setAttribute("productList",list);
+		}
+		return "redirect:/product/story?"+"productId="+list.get(0).getProductId()+"&choose=1&"+request.getQueryString();
+	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Product product,Model model){
