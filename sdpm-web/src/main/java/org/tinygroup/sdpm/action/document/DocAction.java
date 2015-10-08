930d844f1a9fe7cb74ce975254aaf5588e573732
钟起龙 <tsilone@163.com>
2015-10-08 17:56:17 +0800

    比较稳定的版本version 1.0

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/document/DocAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/document/DocAction.java
@@ -2,47 +2,51 @@ package org.tinygroup.sdpm.action.document;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.tinygroup.sdpm.document.dao.pojo.Doc;
 import org.tinygroup.sdpm.document.dao.pojo.Doclib;
 import org.tinygroup.sdpm.document.service.inter.DocService;
 import org.tinygroup.tinysqldsl.Pager;
+import org.tinygroup.weblayer.WebContext;
 
 @Controller
 @RequestMapping(value="/document")
 public class DocAction {
 	@Autowired
 	private DocService docservice;
-	@RequestMapping(value={"","/document"})
-	public String docIndex(Doclib doclib,Model model)
+	@RequestMapping("")
+	public String docIndex(HttpServletRequest request,Model model,String libname)
 	{
-		//List<Doclib> listDoclib = docservice.findDoclibList(doclib);
-		//for(int i=0;i<listDoclib.size();i++)
-		//	listDoclib.get(i);
-		Pager<Doclib> doclibpager = docservice.findDoclibRetPager(0, 20, doclib);
-		model.addAttribute("doclibpager", doclibpager);
-		//doclibpager.getRecords()
+		List<Doclib> list = (List<Doclib>)request.getSession().getAttribute("liblist");
+		if(list==null||list.size()==0){
+			list=docservice.findDoclibList(new Doclib());
+			request.getSession().setAttribute("liblist",list);
+		}
+		else model.addAttribute("libname", libname);
 		return "/document/document.page";
+		//return "redirect:/document/document?"+(list.size()>0?("lib="+list.get(0).getDocLibName()):"");
 	}
 	@RequestMapping(value="/doc/list")
-	public String docList(Doc doc,int limit,Model model,String libname)
+	public String docList(int limit,Model model,String libname)
 	{
-		if(doc==null)
-			doc = new Doc();
+		Doc doc = new Doc();
 		doc.setDocLib(libname);
 		Pager<Doc> docpager = docservice.findDocRetPager(0, limit, doc);
 		model.addAttribute("docpager", docpager);
+		model.addAttribute("libname", libname);
 		return "/data/datalist.pagelet";
 	}
 	@RequestMapping(value="/doc/add")
-	public String createDoc(Doclib doclib,Model model)
+	public String createDoc(Doclib doclib,Model model,String libname)
 	{
-		model.addAttribute("doclib", docservice.findDoclibById(doclib.getDocLibId()));
+		model.addAttribute("libname", libname);
 		return "/document/add-doc.page";
 	}
 	@RequestMapping(value="/doc/edit")
@@ -61,11 +65,12 @@ public class DocAction {
 	@RequestMapping(value="/doc/save",method=RequestMethod.POST)
 	public String saveDoc(Doc doc,Model model)
 	{
-		if(doc.getDocId()!=null||doc.getDocId()!=0)
+		if(doc.getDocId()==null||doc.getDocId()==0)
+			doc = docservice.createNewDoc(doc);
+		else
 			docservice.editDoc(doc);
-		doc = docservice.createNewDoc(doc);
 		model.addAttribute("doc", doc);
-		return "redirect:"+"/document/document";
+		return "redirect:"+"/document?libname="+doc.getDocLib();
 	}
 	@RequestMapping(value="/doc/delete")
 	public String delDoc(Integer docid)
@@ -73,12 +78,12 @@ public class DocAction {
 		/*int ret = */
 		docservice.deleteDocById(docid);
 		//if(ret==0)return "/document/Error.page";
-		return "redirect:"+"/document/document";
+		return "redirect:"+"/document";
 	}
 	@RequestMapping(value="/doclib/add")
 	public String addDocLib()
 	{
-		return "/document/add-doclib.page";
+		return "/document/add-doclib.pagelet";
 	}
 	@RequestMapping(value="/doclib/edit")
 	public String editDoclib(Doclib doclib,Model model,Integer doclibid)
@@ -90,10 +95,11 @@ public class DocAction {
 	@RequestMapping(value="/doclib/save")
 	public String saveDoclib(Doclib doclib)
 	{
-		if(doclib.getDocLibId()!=null||doclib.getDocLibId()!=0)
+		if(doclib.getDocLibId()==null||doclib.getDocLibId()==0)
+			docservice.createNewDocLib(doclib);
+		else
 			docservice.editDocLibName(doclib);
-		docservice.createNewDocLib(doclib);
-		return "redirect:"+"/document/document";
+		return "redirect:"+"/document";
 	}
 	@RequestMapping(value="/doclib/delete")
 	public String delDocLib(Integer doclibid)
@@ -101,6 +107,6 @@ public class DocAction {
 		/*int ret = */
 		docservice.deleteDoclibById(doclibid);
 		//if(ret==0)return "/document/Error.page";
-		return "redirect:"+"/document/document";
+		return "redirect:"+"/document";
 	}
 }
