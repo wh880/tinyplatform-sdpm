79179d6e8d3569b11ace1f6855661f13f321788e
蒙初开 <721787227@qq.com>
2015-10-15 21:19:05 +0800

    修改 action project字段

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/system/ProfileAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/system/ProfileAction.java
@@ -26,7 +26,7 @@ public class ProfileAction extends BaseController{
 	public String eidt(SystemProfile profile,@PathVariable(value="type")String type,Model model){
 		if("doc".equals(type)){
 			profileService.editTitle(profile);
-			return "redirect:"+"/document/doc/view";
+			return "a/document/doc/view";
 		}
 		return null;
 	}
@@ -34,7 +34,7 @@ public class ProfileAction extends BaseController{
 	public String delete(Integer id,@PathVariable(value="type")String type){
 		if("doc".equals(type)){
 			profileService.softDelete(id);
-			return null;
+			return "redirect:"+"a/document/doc/view";
 		}
 		return null;
 	}
