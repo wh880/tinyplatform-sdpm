3a60eb8f6c5a27ecae447487237930381c43bc5a
汪杰 <wangjie14929@hundsun.com>
2015-10-16 08:29:32 +0800

    修改配置

--- sdpm-system/business-impl/src/main/java/org/tinygroup/sdpm/system/biz/impl/ActionManagerImpl.java
+++ sdpm-system/business-impl/src/main/java/org/tinygroup/sdpm/system/biz/impl/ActionManagerImpl.java
@@ -7,53 +7,54 @@ import org.tinygroup.jdbctemplatedslsession.daosupport.OrderBy;
 import org.tinygroup.sdpm.common.util.common.NameUtil;
 import org.tinygroup.sdpm.system.biz.inter.ActionManager;
 import org.tinygroup.sdpm.system.dao.SystemActionDao;
-import org.tinygroup.sdpm.system.dao.impl.ActionEnum;
 import org.tinygroup.sdpm.system.dao.pojo.SystemAction;
 import org.tinygroup.tinysqldsl.Pager;
 
 import java.util.List;
+
 @Service
 @Transactional
 public class ActionManagerImpl implements ActionManager {
-	@Autowired
+    @Autowired
     private SystemActionDao systemActionDao;
-	public SystemAction add(SystemAction systemAction) {
-		return systemActionDao.add(systemAction);
-	}
 
-	public SystemAction updata(SystemAction systemAction) {
-		systemActionDao.edit(systemAction);
-		return systemAction;
-	}
+    public SystemAction add(SystemAction systemAction) {
+        return systemActionDao.add(systemAction);
+    }
 
-	public Integer delete(SystemAction systemAction) {
-		int pk=systemAction.getActionId();
-		return systemActionDao.deleteByKey(pk);
-	}
+    public SystemAction updata(SystemAction systemAction) {
+        systemActionDao.edit(systemAction);
+        return systemAction;
+    }
 
-	public List<SystemAction> find(SystemAction SystemAction) {
-		List<SystemAction> actions = systemActionDao.query(SystemAction);
-		if(actions.size()>0){
-			for(SystemAction s : actions){
-				s = systemActionDao.getActionAndObject(s);
-				s.setUrl(s.getActionObjectType());
-			}
-		}
-		return actions;
-	}
+    public Integer delete(SystemAction systemAction) {
+        int pk = systemAction.getActionId();
+        return systemActionDao.deleteByKey(pk);
+    }
 
-	public Pager<SystemAction> findByPage(int start, int limit, SystemAction systemAction, String order,
-			String ordertype) {
+    public List<SystemAction> find(SystemAction SystemAction) {
+        List<SystemAction> actions = systemActionDao.query(SystemAction);
+        if (actions.size() > 0) {
+            for (SystemAction s : actions) {
+                s = systemActionDao.getActionAndObject(s);
+                s.setUrl(s.getActionObjectType());
+            }
+        }
+        return actions;
+    }
 
-		Pager<SystemAction> pager = systemActionDao.queryPager((start-1)*limit, limit, systemAction, (order==null||"".equals(order))?null:new OrderBy(NameUtil.resolveNameDesc(order), !("desc".equals(ordertype))?true:false));
-		if(pager.getRecords().size()>0){
-			for(SystemAction s : pager.getRecords()){
-				s = systemActionDao.getActionAndObject(s);
-				//s.setUrl(ActionEnum.getUrl(s.getActionObjectType()));			}
-		}
-		return pager;
-	}
+    public Pager<SystemAction> findByPage(int start, int limit, SystemAction systemAction, String order,
+                                          String ordertype) {
 
-	
+        Pager<SystemAction> pager = systemActionDao.queryPager((start - 1) * limit, limit, systemAction, (order == null || "".equals(order)) ? null : new OrderBy(NameUtil.resolveNameDesc(order), !("desc".equals(ordertype)) ? true : false));
+        if (pager.getRecords().size() > 0) {
+            for (SystemAction s : pager.getRecords()) {
+                s = systemActionDao.getActionAndObject(s);
+                //s.setUrl(ActionEnum.getUrl(s.getActionObjectType()));			}
+            }
+        }
+        return pager;
+    }
+
 
 }
