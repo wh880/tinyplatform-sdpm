303b8b23427c740ff84da1798107a6cad1a8eeb2
蒙初开 <721787227@qq.com>
2015-10-18 00:13:42 +0800

    holiday action

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/system/HolidayAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/system/HolidayAction.java
@@ -8,7 +8,6 @@ import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +19,10 @@ import org.tinygroup.sdpm.system.dao.pojo.Holiday;
 import org.tinygroup.sdpm.system.dao.pojo.SystemAction;
 import org.tinygroup.sdpm.system.service.inter.ActionService;
 import org.tinygroup.sdpm.system.service.inter.HolidayService;
+import org.tinygroup.sdpm.util.LogUtil;
+import org.tinygroup.sdpm.util.LogUtil.LogAction;
+import org.tinygroup.sdpm.util.LogUtil.LogOperateObject;
+import org.tinygroup.sdpm.util.UserUtils;
 import org.tinygroup.tinysqldsl.Pager;
 @Controller
 @RequestMapping("a/system")
@@ -69,6 +72,7 @@ public class HolidayAction extends BaseController{
 	}
 	@RequestMapping(value ="holiday/save",method = RequestMethod.POST)
 	public String saveHoliday(@RequestParam(required = false)String selectList,Holiday holiday,Model model){
+		SystemAction action = new SystemAction();
 		if(holiday.getHolidayId()==null){
 			List<Holiday> holidayList = new ArrayList<Holiday>();
 			String[] dates=selectList.split(",");
@@ -90,8 +94,15 @@ public class HolidayAction extends BaseController{
 			}
 			
 			holidayService.batchAdd(holidayList);
+			LogUtil.logWithComment(LogUtil.LogOperateObject.HOLIDAY,
+					LogUtil.LogAction.OPENED, null,
+					UserUtils.getUserId(), null, null, null, null, null);
 		}else{
+			
 			holidayService.update(holiday);
+			LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.EDITED, 
+					String.valueOf(holiday.getHolidayId()), 
+					UserUtils.getUserId(), null, null, null, null, null);
 		}
 		model.addAttribute("holiday", holiday);
 		return "/system/page/holiday/holiday.page";
@@ -106,6 +117,9 @@ public class HolidayAction extends BaseController{
 		   holidayService.delete(holiday);
 		   map.put("info", "删除成功");
 		   map.put("status", "y");
+		   LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.DELETED, 
+				   String.valueOf(id), UserUtils.getUserAccount(), null, 
+				   null, null, null, null);
 	     }
 	   else{
 		   map.put("info", "删除失败");
@@ -144,7 +158,11 @@ public class HolidayAction extends BaseController{
 	public String batchDelete(Holidays holidays)
 	{
 	   List<Holiday> holiday= holidays.getHoliday();
-	 
+	    for(int i=0,n=holiday.size();i<n;i++){
+	    	 LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.DELETED, 
+					   String.valueOf(holiday.get(i).getHolidayId()), UserUtils.getUserAccount(), null, 
+					   null, null, null, holiday.get(i).getHoilidayRemark());
+	    }
 		holidayService.batchSofeDelete(holiday);
 		return  "/system/page/holiday/holiday.page";
 	}
