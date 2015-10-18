9aef863c8496d0db3282153237b3cb209fdddedc
蒙初开 <721787227@qq.com>
2015-10-18 18:18:26 +0800

    修改

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/BugAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/quality/BugAction.java
@@ -12,7 +12,6 @@ import org.tinygroup.commons.tools.StringUtil;
 import org.tinygroup.sdpm.action.product.util.StoryUtil;
 import org.tinygroup.sdpm.action.quality.util.QualityUtil;
 import org.tinygroup.sdpm.action.system.ProfileUtil;
-import org.tinygroup.sdpm.action.system.Profiles;
 import org.tinygroup.sdpm.common.util.ComplexSearch.SearchInfos;
 import org.tinygroup.sdpm.common.web.BaseController;
 import org.tinygroup.sdpm.org.dao.pojo.OrgUser;
