6470725378a328df1da44745686b9160944c905d
明明 <563936043@qq.com>
2015-10-19 19:40:48 +0800

    完成协议里面的历史记录

---
+++ sdpm-service/dao-impl/src/main/java/org/tinygroup/sdpm/service/dao/impl/FieldUtil.java
@@ -0,0 +1,8 @@
+package org.tinygroup.sdpm.service.dao.impl;
+
+public class FieldUtil {
+
+    public static final int DELETE_NO = 0;
+    public static final int DELETE_YES = 1;
+
+}
--- sdpm-service/dao-interface/src/main/java/org/tinygroup/sdpm/service/dao/pojo/ServiceSla.java
+++ sdpm-service/dao-interface/src/main/java/org/tinygroup/sdpm/service/dao/pojo/ServiceSla.java
@@ -24,8 +24,8 @@ import java.util.Date;
  */
 public class ServiceSla {
 
-	public static Integer DELETE_YES=1;
-	public static Integer DELETE_NO=0;
+	//	public static Integer DELETE_YES=1;
+//	public static Integer DELETE_NO=0;
 	/*在faq的pojo中，添加客户名称*/
 	private String clientName;
 	private String productName;
@@ -45,8 +45,6 @@ public class ServiceSla {
 	 * 3vip
 	 */
 	private Integer serviceLevel;
-	/*关联客户表的客户名称*/
-	private  Integer  serviceName;
 	/**
 	 * 服务期限
 	 *
@@ -161,7 +159,7 @@ public class ServiceSla {
 	private String cilentProductVision;
 
 	public ServiceSla() {
-		setDeleted(DELETE_NO);
+		setDeleted(0);
 	}
 
 	public String getClientName() {
