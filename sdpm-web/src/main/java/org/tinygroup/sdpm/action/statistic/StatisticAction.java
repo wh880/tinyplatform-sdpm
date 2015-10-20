8be33d7384087c4f3afebffeead4d0e44d55f8af
蒙初开 <721787227@qq.com>
2015-10-20 13:01:49 +0800

    修改

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/statistic/StatisticAction.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/statistic/StatisticAction.java
@@ -1,9 +1,19 @@
 package org.tinygroup.sdpm.action.statistic;
 
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.tinygroup.sdpm.common.web.BaseController;
+import org.tinygroup.sdpm.product.dao.pojo.Product;
+import org.tinygroup.sdpm.product.dao.pojo.ProductPlan;
+import org.tinygroup.sdpm.product.service.PlanService;
+import org.tinygroup.sdpm.product.service.ProductService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by MCK on 2015/10/19.
@@ -11,8 +21,27 @@ import org.tinygroup.sdpm.common.web.BaseController;
 @Controller
 @RequestMapping("a/statistic")
 public class StatisticAction extends BaseController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private PlanService planService;
     @RequestMapping("index")
     public String index (Model model){
-         return "/statistic/page/product.page";
+
+        return "/statistic/page/product.page";
+    }
+    @RequestMapping("product/all")
+    public String productAll(Model model){
+        List<Product> products=productService.findProductList(new Product());
+        Map<Product,List<ProductPlan>> map=  new HashMap<Product, List<ProductPlan>>();
+        for(int i=0,n=products.size();i<n;i++){
+            ProductPlan plan =new ProductPlan();
+            plan.setProductId(products.get(i).getProductId());
+            List<ProductPlan> productPlans = planService.findPlanList(plan);
+            productPlans.size()
+            map.put(products.get(i),productPlans);
+        }
+        model.addAttribute("product",map);
+        return "/statistic/data/productData1.pagelet";
     }
 }
