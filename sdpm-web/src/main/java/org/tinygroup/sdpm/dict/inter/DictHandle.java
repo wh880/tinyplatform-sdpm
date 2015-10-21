4113337632e282914b8f6bdb4cb1af71e87b88f8
Hulk <jayzch@qq.com>
2015-10-21 11:56:08 +0800

    修改shiro配置

--- sdpm-web/src/main/java/org/tinygroup/sdpm/dict/inter/DictHandle.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/dict/inter/DictHandle.java
@@ -6,15 +6,13 @@ import java.util.List;
  * Created by wangll13383 on 2015/10/15.
  */
 public interface DictHandle {
-    public static String DICT_XSTREAM= "dictFile";
+    String DICT_XSTREAM = "dictFile";
 
-    public void addDictEntry(DictNodeEntries dictNodeEntries, String filePath) throws Exception;
+    void addDictEntry(DictNodeEntries dictNodeEntries, String filePath) throws Exception;
 
-    public DictNodeEntry getDict(String key);
+    void insertDictNodeEntry();
 
-    public void insertDictNodeEntry();
+    List<DictNodeEntries> getDictNodeEntries();
 
-    public List<DictNodeEntries> getDictNodeEntries();
-
-    public void removeDictNodeEntries(DictNodeEntries dictNodeEntries);
+    void removeDictNodeEntries(DictNodeEntries dictNodeEntries);
 }
