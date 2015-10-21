8880749a9124cdcba670657f7501f787c1336d24
王莹 <wymsmdbl@163.com>
2015-10-21 15:26:11 +0800

    批量关联需求

--- sdpm-project/dao-interface/src/main/java/org/tinygroup/sdpm/project/dao/ProjectBuildDao.java
+++ sdpm-project/dao-interface/src/main/java/org/tinygroup/sdpm/project/dao/ProjectBuildDao.java
@@ -39,4 +39,6 @@ public interface ProjectBuildDao extends BaseDao<ProjectBuild, Integer> {
     public Pager<ProductStory> findnoBuildStorys(int start, int limit, Integer buildId, OrderBy... orderArgs);
 
     public Integer deletereleate(Integer storyId,Integer buildId);
+
+    public Integer releateReq(Integer storyId,Integer buildId);
 }
