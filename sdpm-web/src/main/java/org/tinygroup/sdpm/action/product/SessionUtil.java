c71f38bdbaee8e88dc31dea77fb0944aee80f8e1
xubinbin <1835094758@qq.com>
2015-10-22 16:09:37 +0800

    修改

--- sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/SessionUtil.java
+++ sdpm-web/src/main/java/org/tinygroup/sdpm/action/product/SessionUtil.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 
 import org.tinygroup.sdpm.product.dao.pojo.Product;
 import org.tinygroup.sdpm.product.service.ProductService;
+import org.tinygroup.sdpm.productLine.dao.pojo.ProductLine;
+import org.tinygroup.sdpm.productLine.service.ProductLineService;
 
 public class SessionUtil {
 	
@@ -15,9 +17,22 @@ public class SessionUtil {
 		Product product = new Product();
 		if(session.getAttribute("sessionProductLineId")!=null){
 			product.setProductLineId((Integer)request.getSession().getAttribute("sessionProductLineId"));
+		}else{
+			product.setProductLineId(null);
 		}
 		List<Product> list = productService.findProductList(product,"productId","desc");
 		request.getSession().setAttribute("productList",list);
+		return list;
+		
+	}
+	
+	public static List<ProductLine> ProductLineRefresh(HttpServletRequest request,ProductLineService productLineService){
+		HttpSession session = request.getSession();
+		List<ProductLine> list = productLineService.findProductLineList(new ProductLine(), "productLineId", "desc");
+		request.getSession().setAttribute("productLineList",list);
+		if(session.getAttribute("sessionProductLineId")==null){
+			session.setAttribute("sessionProductLineId", list.size()>0?list.get(0).getProductLineId():null);
+		}
 		return list;
 		
 	}
