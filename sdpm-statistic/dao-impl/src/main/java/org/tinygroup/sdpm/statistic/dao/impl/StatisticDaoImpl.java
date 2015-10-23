fa15eb8d243f7bf32c08ebfd73d092231a3410c3
蒙初开 <721787227@qq.com>
2015-10-23 11:49:24 +0800

    修改case 字典  statistic 统计Bug对象

--- sdpm-statistic/dao-impl/src/main/java/org/tinygroup/sdpm/statistic/dao/impl/StatisticDaoImpl.java
+++ sdpm-statistic/dao-impl/src/main/java/org/tinygroup/sdpm/statistic/dao/impl/StatisticDaoImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 import org.tinygroup.jdbctemplatedslsession.daosupport.TinyDslDaoSupport;
 import org.tinygroup.sdpm.statistic.dao.StatisticDao;
 import org.tinygroup.sdpm.statistic.dao.pojo.ProjectTaskSta;
+import org.tinygroup.sdpm.statistic.dao.pojo.QualityBugSta;
 import org.tinygroup.sdpm.statistic.dao.pojo.StatisticOrg;
 import org.tinygroup.tinysqldsl.Select;
 import org.tinygroup.tinysqldsl.extend.MysqlSelect;
@@ -12,8 +13,12 @@ import org.tinygroup.tinysqldsl.selectitem.FragmentSelectItemSql;
 
 import java.util.List;
 
+import static org.tinygroup.sdpm.org.dao.constant.OrgUserTable.ORG_USERTABLE;
 import static org.tinygroup.sdpm.project.dao.constant.ProjectTable.PROJECTTABLE;
 import static org.tinygroup.sdpm.project.dao.constant.ProjectTaskTable.PROJECT_TASKTABLE;
+import static org.tinygroup.sdpm.quality.dao.constant.QualityBugTable.QUALITY_BUGTABLE;
+import static org.tinygroup.tinysqldsl.base.StatementSqlBuilder.and;
+import static org.tinygroup.sdpm.product.dao.constant.ProductStoryTable.PRODUCT_STORYTABLE;
 @Repository
 public class StatisticDaoImpl extends TinyDslDaoSupport implements StatisticDao{
 
@@ -43,9 +48,33 @@ public class StatisticDaoImpl extends TinyDslDaoSupport implements StatisticDao{
 				"SUM(project_task.task_id) AS taskNum," +
 				"SUM(project_task.task_estimate) AS estimate," +
 				"SUM(project_task.task_left) AS 'left'")).from(PROJECT_TASKTABLE).join(Join.leftJoin(PROJECTTABLE,
-				PROJECT_TASKTABLE.TASK_PROJECT.eq(PROJECTTABLE.PROJECT_ID))).where(PROJECT_TASKTABLE.TASK_ASSIGNED_TO.eq(
-				projectTaskSta.getAssignedTo())).groupBy(PROJECTTABLE.PROJECT_ID);
+				PROJECT_TASKTABLE.TASK_PROJECT.eq(PROJECTTABLE.PROJECT_ID)),Join.leftJoin(ORG_USERTABLE,
+				PROJECT_TASKTABLE.TASK_ASSIGNED_TO.eq(ORG_USERTABLE.ORG_USER_ID))).where(and(PROJECT_TASKTABLE.TASK_ASSIGNED_TO.eq(
+				projectTaskSta.getAssignedTo()),
+				PROJECT_TASKTABLE.TASK_ASSIGNED_DATE.eq(projectTaskSta.getAssignedDate()),
+				ORG_USERTABLE.ORG_USER_ID.eq(projectTaskSta.getAssignedTo()))
+		).groupBy(PROJECTTABLE.PROJECT_ID);
 		return getDslSession().fetchList(select,ProjectTaskSta.class);
+	}
+	public List<QualityBugSta> findBugCreate(QualityBugSta qualityBugSta){
+		if (qualityBugSta.getUserId()==null){
+			return null;
+		}
+		Select select = MysqlSelect.select(FragmentSelectItemSql.fragmentSelect("org_user.org_user_id AS userId," +
+				"SUM( CASE WHEN quality_bug.bug_status = 1 THEN 1 ELSE 0 END ) repeatBug,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 2 THEN	1	ELSE	0	END	) designEd,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 3 THEN	1	ELSE	0	END	) externalCause,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 4 THEN	1	ELSE	0	END	) solved,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 5 THEN	1	ELSE	0	END	) notReproduce,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 6 THEN	1	ELSE	0	END	) delayResolved,"+
+				"SUM(CASE WHEN quality_bug.bug_status = 7 THEN	1	ELSE	0	END	) notResolved,"+
+				"COUNT(product_story.story_from_bug) AS toStroy")).from(QUALITY_BUGTABLE).join(Join.leftJoin(
+				ORG_USERTABLE,ORG_USERTABLE.ORG_USER_ID.eq(QUALITY_BUGTABLE.BUG_OPENED_BY)
+		),Join.leftJoin(PRODUCT_STORYTABLE,PRODUCT_STORYTABLE.STORY_FROM_BUG.eq(QUALITY_BUGTABLE.BUG_ID))).where(and(
+				QUALITY_BUGTABLE.BUG_OPENED_DATE.eq(qualityBugSta.getBugCreateDate()),
+				QUALITY_BUGTABLE.PRODUCT_ID.eq(qualityBugSta.getProductId()),
+				QUALITY_BUGTABLE.PROJECT_ID.eq(qualityBugSta.getProjectId()))).groupBy(ORG_USERTABLE.ORG_USER_ID);
+		return getDslSession().fetchList(select,QualityBugSta.class);
 	}
 
 
