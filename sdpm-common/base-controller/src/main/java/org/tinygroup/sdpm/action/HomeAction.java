a1c94a0484128cf52677daa2654ec13976dd862c
Hulk <jayzch@qq.com>
2016-02-13 00:18:43 +0800

    调整项目结构，增加controller层

---
+++ sdpm-common/base-controller/src/main/java/org/tinygroup/sdpm/action/HomeAction.java
@@ -0,0 +1,96 @@
+package org.tinygroup.sdpm.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.tinygroup.sdpm.common.web.BaseController;
+import org.tinygroup.sdpm.product.dao.pojo.Product;
+import org.tinygroup.sdpm.product.service.inter.ProductService;
+import org.tinygroup.sdpm.project.dao.pojo.Project;
+import org.tinygroup.sdpm.project.dao.pojo.ProjectTask;
+import org.tinygroup.sdpm.project.service.dto.BurnDTO;
+import org.tinygroup.sdpm.project.service.inter.BurnService;
+import org.tinygroup.sdpm.project.service.inter.ProjectService;
+import org.tinygroup.sdpm.project.service.inter.TaskService;
+import org.tinygroup.sdpm.quality.dao.pojo.QualityBug;
+import org.tinygroup.sdpm.quality.dao.pojo.QualityTestRun;
+import org.tinygroup.sdpm.quality.service.inter.BugService;
+import org.tinygroup.sdpm.quality.service.inter.TestRunService;
+import org.tinygroup.tinysqldsl.Pager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wangll13383 on 2015/11/16.
+ */
+@Controller
+public class HomeAction extends BaseController{
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private BurnService burnService;
+    @Autowired
+    private ProjectService projectService;
+    @Autowired
+    private BugService bugService;
+    @Autowired
+    private TestRunService testRunService;
+
+    @RequestMapping(value = {"a", "a/home"})
+    public String index(Model model) {
+        //首页产品统计展示
+        List<Product> products = productService.getProductByUserWithCount(userUtils.getUserId(), 0, false,null);
+
+        Map<String, Map<String, String>> productSum = new HashMap<String, Map<String, String>>();
+        for (Product product : products) {
+            Map<String, String> sumMap = new HashMap<String, String>();
+            sumMap.put("activeSum", String.valueOf(product.getActiveSum()==null?0:product.getActiveSum()));
+            sumMap.put("assignSum", String.valueOf(product.getAssignSum()==null?0:product.getAssignSum()));
+            sumMap.put("changedSum", String.valueOf(product.getChangeSum()==null?0:product.getChangeSum()));
+            sumMap.put("bugCount", String.valueOf(product.getBugCount()==null?0:product.getBugCount()));
+            sumMap.put("closeSum", String.valueOf(product.getCloseSum()==null?0:product.getCloseSum()));
+            sumMap.put("draftSum", String.valueOf(product.getDraftSum()==null?0:product.getDraftSum()));
+            sumMap.put("planCount", String.valueOf(product.getPlanCount()==null?0:product.getPlanCount()));
+            sumMap.put("releaseCount", String.valueOf(product.getReleaseCount()==null?0:product.getReleaseCount()));
+            sumMap.put("resolvedSum", String.valueOf(product.getResolveSum()==null?0:product.getResolveSum()));
+            productSum.put(String.valueOf(product.getProductId()==null?0:product.getProductId()), sumMap);
+        }
+        model.addAttribute("productList", products);
+        model.addAttribute("productSumMap", productSum);
+        //首页指派给我的任务
+        ProjectTask task = new ProjectTask();
+        task.setTaskAssignedTo(userUtils.getUserId());
+        task.setTaskDeleted(ProjectTask.DELETE_NO);
+        task.setTaskStatus(ProjectTask.DOING);
+        List<ProjectTask> tasks = taskService.findListTask(task);
+        model.addAttribute("myTaskList", tasks);
+        //首页项目统计
+        Integer[] userProjectIds = projectOperate.getUserProjectIdList();
+        Pager<Project> projectPager = projectService.findProjects(0, userProjectIds != null ? userProjectIds.length:0, "project_id", "false", userProjectIds);
+        if (projectPager.getRecords().size() > 0) {
+            for (Project project : projectPager.getRecords()) {
+                BurnDTO burnDTO = burnService.initBurn(project.getProjectId(), null);
+                project.setBurnValue(burnDTO.getLeftValues());
+            }
+        }
+        model.addAttribute("myProjectList", projectPager.getRecords());
+        //首页指派给我的bug
+        QualityBug bug = new QualityBug();
+        bug.setDeleted(0);
+        bug.setBugAssignedTo(userUtils.getUserId());
+        bug.setBugStatus(QualityBug.STATUS_ACTIVE);
+        List<QualityBug> bugList = bugService.findBugList(bug);
+        model.addAttribute("myBugList", bugList);
+        //首页指派给我的用例
+        QualityTestRun run = new QualityTestRun();
+        run.setTestRunAssignedTo(userUtils.getUserId());
+        List<QualityTestRun> testRuns = testRunService.findTestRunList(run);
+        model.addAttribute("runList", testRuns);
+        return "main/index.page";
+    }
+}
---
+++ sdpm-system/controller/src/main/java/org/tinygroup/sdpm/action/system/HolidayAction.java
@@ -0,0 +1,190 @@
+package org.tinygroup.sdpm.action.system;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.tinygroup.sdpm.common.web.BaseController;
+import org.tinygroup.sdpm.dto.system.Holidays;
+import org.tinygroup.sdpm.org.service.inter.UserService;
+import org.tinygroup.sdpm.system.dao.pojo.Holiday;
+import org.tinygroup.sdpm.system.service.inter.HolidayService;
+import org.tinygroup.sdpm.util.LogUtil;
+import org.tinygroup.sdpm.util.LogUtil.LogAction;
+import org.tinygroup.sdpm.util.LogUtil.LogOperateObject;
+import org.tinygroup.tinysqldsl.Pager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("a/system")
+public class HolidayAction extends BaseController {
+    @Autowired
+    private HolidayService holidayService;
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("")
+    public String index() {
+        return "/system/page/holiday/holiday.page";
+    }
+
+    @RequestMapping("holiday")
+    public String holiday() {
+        return "/system/page/holiday/holiday.page";
+    }
+
+    @RequestMapping("holiday/add")
+    public String holidayAdd() {
+        return "/system/page/holiday/holiday-add.pagelet";
+    }
+
+    @RequestMapping("holiday/findPager")
+    public String fingPage(Integer start, Integer limit,
+                           @RequestParam(required = false,defaultValue = "holidayId")String order,
+                           @RequestParam(required = false,defaultValue = "desc")String ordertype, Holiday holiday, Model model) {
+        boolean asc = true;
+        if ("desc".equals(ordertype)) {
+            asc = false;
+        }
+        holiday.setHolidayDeleted(0);
+        Pager<Holiday> holidayPage = holidayService.findByPage(start, limit, holiday, order, asc);
+        model.addAttribute("holiday", holidayPage);
+        return "/system/page/holiday/data/holidaydata.pagelet";
+    }
+
+    @RequestMapping("holiday/find")
+    public String find(Holiday holiday, Model model) {
+        Holiday day = holidayService.findHolidayById(holiday.getHolidayId());
+        model.addAttribute("holiday", day);
+        return "/system/page/holiday/edit.page";
+    }
+
+    @RequestMapping("holiday/view")
+    public String view(Holiday holiday, Model model) {
+        Holiday day = holidayService.findHolidayById(holiday.getHolidayId());
+        model.addAttribute("holiday", day);
+        return "/system/page/holiday/view.page";
+    }
+
+    @RequestMapping(value = "holiday/save", method = RequestMethod.POST)
+    public String saveHoliday(@RequestParam(required = false) String selectList, Holiday holiday, Model model) {
+        if (holiday.getHolidayId() == null) {
+            List<Holiday> holidayList = new ArrayList<Holiday>();
+            if(!StringUtils.isBlank(selectList)) {
+                String[] dates = selectList.split(",");
+                for (int i = 0, n = dates.length; i < n; i++) {
+                    Holiday day = new Holiday();
+                    day.setCompanyId(holiday.getCompanyId());
+                    day.setHoilidayRemark(holiday.getHoilidayRemark());
+                    day.setHolidayAccount(userUtils.getUserId());
+                    day.setHolidayDate(dates[i]);
+                    day.setHolidayDeleted(holiday.getHolidayDeleted());
+                    day.setHolidayDetail(holiday.getHolidayDetail());
+                    day.setHolidayName(holiday.getHolidayName());
+                    day.setHolidayType(holiday.getHolidayType());
+                    holidayList.add(day);
+                }
+            }
+            List<Holiday> holidays = holidayService.batchAddHoliday(holidayList);
+            for (int i = 0, n = holidays.size(); i < n; i++) {
+                LogUtil.logWithComment(LogOperateObject.HOLIDAY,
+                        LogAction.OPENED, String.valueOf(holidays.get(i).getHolidayId()),
+                        userUtils.getUserId(), null, null, null, null, null);
+            }
+        } else {
+            Holiday holiday1 = holidayService.findHolidayById(holiday.getHolidayId());
+            holidayService.updateHoliday(holiday);
+            LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.EDITED,
+                    String.valueOf(holiday.getHolidayId()),
+                    userUtils.getUserId(), null, null, holiday1, holiday, null);
+        }
+        model.addAttribute("holiday", holiday);
+        return "redirect:/a/system";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "holiday/delete")
+    public Map<String, String> deleteHoliday(Integer id) {
+        Map<String, String> map = new HashedMap();
+        if (id != null) {
+            Holiday holiday = new Holiday();
+            holiday.setHolidayId(id);
+            holidayService.deleteHoliday(holiday);
+            map.put("info", "删除成功");
+            map.put("status", "y");
+            LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.DELETED,
+                    String.valueOf(id), userUtils.getUserId(), null,
+                    null, null, null, null);
+        } else {
+            map.put("info", "删除失败");
+            map.put("status", "n");
+        }
+        return map;
+    }
+
+    @RequestMapping("holiday/manage")
+    public String manage(Holiday holiday, Model model) {
+        List<Holiday> holidayList = holidayService.findHolidayList(holiday);
+        model.addAttribute("holiday", holidayList);
+        return "/system/page/holiday/manage.page";
+    }
+
+    @RequestMapping("holiday/findIds")
+    public String findIds(String ids, Model model) {
+        String[] sids = ids.split(",");
+        Integer[] intIds = new Integer[sids.length];
+        for (int i = 0; i < sids.length; i++) {
+            intIds[i] = Integer.valueOf(sids[i]);
+        }
+        List<Holiday> holidayList = holidayService.findHolidayByIds(intIds);
+        model.addAttribute("holiday", holidayList);
+        return "/system/page/holiday/batch-del.pagelet";
+    }
+
+    @RequestMapping("holiday/batchDelete")
+    public String batchDelete(Holidays holidays) {
+        List<Holiday> holiday = holidays.getHoliday();
+        for (int i = 0, n = holiday.size(); i < n; i++) {
+            LogUtil.logWithComment(LogOperateObject.HOLIDAY, LogAction.DELETED,
+                    String.valueOf(holiday.get(i).getHolidayId()), userUtils.getUserId(), null,
+                    null, null, null, holiday.get(i).getHoilidayRemark());
+        }
+        holidayService.batchSoftDeleteHoliday(holiday);
+        return "/system/page/holiday/holiday.page";
+    }
+
+    @RequestMapping("holiday/action")
+    public String holidayAction(Model model) {
+        Holiday holiday = new Holiday();
+        Pager<Holiday> historyPage = holidayService.findByPage(0, 7, holiday, "systemAction.actionDate", false);
+        List<Holiday> histories = historyPage.getRecords();
+        model.addAttribute("action", histories);
+        return "/system/page/holiday/holiday-dynamic.pagelet";
+    }
+
+    @RequestMapping("holiday/history/more")
+    public String historyMore() {
+        return "/system/page/holiday/holidayHistory.pagelet";
+    }
+
+    @RequestMapping("holiday/history")
+    public String holidayHistory(Integer start, Integer limit, String order, String ordertype, Model model) {
+        Holiday holiday = new Holiday();
+        boolean asc = true;
+        if ("desc".equals(ordertype)) {
+            asc = false;
+        }
+        Pager<Holiday> History = holidayService.findByPage(start, limit, holiday, "action_date", false);
+
+        model.addAttribute("History", History);
+        return "/system/page/holiday/holidayHistoryTableData.pagelet";
+    }
+}
