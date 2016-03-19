ca0078be849b945ff0259980f56e1c450c60f53d
Hulk <jayzch@qq.com>
2016-03-19 15:30:07 +0800

    修正需求关闭导致的问题

--- sdpm-common/base-controller/src/main/java/org/tinygroup/sdpm/security/filter/FormAuthenticationFilter.java
+++ sdpm-common/base-controller/src/main/java/org/tinygroup/sdpm/security/filter/FormAuthenticationFilter.java
@@ -1,6 +1,3 @@
-/**
- * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
- */
 package org.tinygroup.sdpm.security.filter;
 
 import org.apache.shiro.authc.AuthenticationException;
