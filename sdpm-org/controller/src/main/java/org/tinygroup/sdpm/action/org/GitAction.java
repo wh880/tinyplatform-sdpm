74caa55a4e8108c9bdeae72e81ac895e8377c538
Hulk <jayzch@qq.com>
2016-03-31 16:19:33 +0800

    调整git钩子

--- sdpm-org/controller/src/main/java/org/tinygroup/sdpm/action/org/GitAction.java
+++ sdpm-org/controller/src/main/java/org/tinygroup/sdpm/action/org/GitAction.java
@@ -3,7 +3,6 @@ package org.tinygroup.sdpm.action.org;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.tinygroup.convert.objectjson.fastjson.JsonToObject;
 import org.tinygroup.logger.LogLevel;
@@ -14,10 +13,6 @@ import org.tinygroup.sdpm.org.dao.pojo.OrgUser;
 import org.tinygroup.sdpm.org.service.inter.GitService;
 import org.tinygroup.sdpm.org.service.inter.UserService;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,29 +29,12 @@ public class GitAction extends BaseController {
     private UserService userService;
 
     @ResponseBody
-    @RequestMapping(value = "/convert", method = RequestMethod.POST)
-    public void convertToJava(HttpServletRequest req) {
-        logger.logMessage(LogLevel.INFO, "开始解析报文");
-        int size = req.getContentLength();
-        if (size > 0) {
-            try {
-                BufferedReader reader = req.getReader();
-                StringBuffer jsonStringBuffer = new StringBuffer();
-                String temp;
-                while ((temp = reader.readLine()) != null) {
-                    jsonStringBuffer.append(temp);
-                }
-                String jsonString = URLDecoder.decode(jsonStringBuffer.toString(), "utf-8");
-                jsonString = jsonString.replaceFirst("hook=", "");
-                reader.close();
-                logger.logMessage(LogLevel.INFO, "报文内容：{}", jsonString);
-                JsonToObject<Hook> jsonToObject = new JsonToObject<Hook>(Hook.class);
-                Hook jsonObject = jsonToObject.convert(jsonString);
-                addCommitInfo(jsonObject);
-            } catch (IOException e) {
-                logger.logMessage(LogLevel.ERROR, "convert error", e);
-            }
-        }
+    @RequestMapping(value = "/pull")
+    public void pull(String hook) {
+        logger.logMessage(LogLevel.INFO, "pull开始解析报文{}",hook);
+        JsonToObject<Hook> jsonToObject = new JsonToObject<Hook>(Hook.class);
+        Hook jsonObject = jsonToObject.convert(hook);
+        addCommitInfo(jsonObject);
         logger.logMessage(LogLevel.INFO, "结束报文解析");
     }
 
