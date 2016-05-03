5b6babf712420e25b496bdcee5c99eeebb7fc6bb
dingzanzan <1603069224@qq.com>
2016-05-03 11:01:12 +0800

    修改 1.项目→任务→点击左侧模块上级的产品，显示系统错误 2.项目→新建项目:对项目名称和项目代码输入长度验证

--- sdpm-project/controller/src/main/java/org/tinygroup/sdpm/action/project/ProjectCommonAction.java
+++ sdpm-project/controller/src/main/java/org/tinygroup/sdpm/action/project/ProjectCommonAction.java
@@ -1,9 +1,11 @@
 package org.tinygroup.sdpm.action.project;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.tinygroup.commons.tools.StringUtil;
 import org.tinygroup.sdpm.common.web.BaseController;
 import org.tinygroup.sdpm.project.service.inter.ProjectService;
 
@@ -30,6 +32,10 @@ public class ProjectCommonAction extends BaseController {
 
     @RequestMapping("/selModelTask")
     public String selModelTask(String moduleId) {
+        if(!StringUtil.isBlank(moduleId)&&StringUtil.contains(moduleId,"p"))
+        {
+            return "redirect:" + adminPath + "/project/task/index?choose=1";
+        }
         return "redirect:" + adminPath + "/project/task/index?moduleId=" + moduleId;
     }
 
