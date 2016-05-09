ff3b52a9bb4b52a59e6e75f650c55231a42a9a70
Hulk <jayzch@qq.com>
2016-05-09 10:23:41 +0800

    调整依赖

--- sdpm-quality/controller/src/main/java/org/tinygroup/sdpm/action/quality/CaseStepAction.java
+++ sdpm-quality/controller/src/main/java/org/tinygroup/sdpm/action/quality/CaseStepAction.java
@@ -1,12 +1,12 @@
 package org.tinygroup.sdpm.action.quality;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.tinygroup.sdpm.common.web.BaseController;
 import org.tinygroup.sdpm.quality.dao.pojo.QualityCaseStep;
 import org.tinygroup.sdpm.quality.service.inter.CaseStepService;
-import org.tinygroup.weblayer.mvc.annotation.Controller;
 
 /**
  * Created by chenpeng15668 on 2015-9-24
