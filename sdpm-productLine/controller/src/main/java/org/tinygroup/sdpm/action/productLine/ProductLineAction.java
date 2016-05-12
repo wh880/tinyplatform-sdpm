c5530d239114714a9ac72a4ee6a35c43c3d8b2cc
xuyw16862 <792370893@qq.com>
2016-05-12 14:41:07 +0800

    146.产品→概况→点击右边的产品线信息会报error 500

--- sdpm-productLine/controller/src/main/java/org/tinygroup/sdpm/action/productLine/ProductLineAction.java
+++ sdpm-productLine/controller/src/main/java/org/tinygroup/sdpm/action/productLine/ProductLineAction.java
@@ -196,10 +196,19 @@ public class ProductLineAction extends BaseController {
 
     @RequestMapping("/find/{forword}")
     public String find(@CookieValue(value = "cookieProductLineId", defaultValue = "0") String cookieProductLineId,
+                       @CookieValue(value = "cookieProductId", defaultValue = "0") String cookieProductId,
                        @PathVariable(value = "forword") String forword, Integer productId, Integer productLineId, Model model, HttpServletRequest request) {
 
         if (productLineId == null) {
-            productLineId = Integer.parseInt(cookieProductLineId);
+            if (Integer.parseInt(cookieProductId) > 0) {
+                Product product = new Product();
+                product = productService.findProductById(Integer.parseInt(cookieProductId));
+                productLineId = product.getProductLineId();
+                if (product.getProductId() == null) {
+                    return notFoundView();
+                }
+            }
+            //productLineId = Integer.parseInt(cookieProductLineId);
         }
 
         ProductLine productLine = productLineService.findProductLine(productLineId);
